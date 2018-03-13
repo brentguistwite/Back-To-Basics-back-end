bc5aac74c6887291e8144220d60beabfc04ed5f6
Brent Guistwite <brentguistwite@gmail.com>
2018-03-13 14:11:29 -0400

    add endpoint to test database

---
+++ questions/router.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const { Question, } = require('./models');
+
+const router = express.Router();
+
+// Using this to test database
+
+router.get('/', (req, res) => {
+  Question
+    .find()
+    .then(questions => res.status(201).json(questions))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).json({ message: 'Internal Server Error', });
+    });
+});
+
+module.exports = router;
